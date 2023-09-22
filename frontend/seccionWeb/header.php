8efc298db633f719032fe141262fd5e04e49d5ca
AustinAMTZ16 <austintv52@gmail.com>
2023-09-22 13:40:43 -0600

    Update: Cambio de diseño de pagina web

---
+++ frontend/seccionWeb/header.php
@@ -0,0 +1,33 @@
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand" href="index.php?view=home"><img src="BackWeb/assets/img/solmty.png" alt="..." /></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fas fa-bars ms-1"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                <li class="nav-item"><a class="nav-link" href="index.php?view=home">Inicio</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php?view=home">Acerca</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php?view=home">Ecommerce</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php?view=home">Web Site</a></li>
+                <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        SEO
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="index.php?view=home">Bussines Intelligent</a></li>
+                        <li><a class="dropdown-item" href="index.php?view=home">Formularios web</a></li>
+                        <li><a class="dropdown-item" href="index.php?view=home">ChatBoot WhatsApp</a></li>
+                        
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="index.php?view=home">CRM Intelligent</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="index.php?view=contacto">Contacto</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
