4be0867168179021e46950225215cd90e04cbeb1
AustinAMTZ16 <austintv52@gmail.com>
2023-09-23 11:09:29 -0600

    Update: Pagina y sistema de reservacion

--- frontend/seccionWeb/servicios.php
+++ frontend/seccionWeb/servicios.php
@@ -6,142 +6,104 @@
         <div >
             <div class="">
                 <div class="modal-content">
-                    <div class="close-modal" data-bs-dismiss="modal">
-                        <img src="./assents/logo.jpg" alt="Close modal" /></div>
+                    <!-- <div class="close-modal" data-bs-dismiss="modal">
+                        <img src="./assents/logo.jpg" alt="Close modal" /></div> -->
                     <div class="container">
                         <h2 class="text-uppercase">TODOS LOS SERVICIOS:</h2>
                         <div class="row justify-content-center">
                             <div class="col-lg-4">
                                 <div class="modal-body">
-                                    <h2 class="text-uppercase">Project Name</h2>
-                                    <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                                    <img class="img-fluid d-block mx-auto" src="BackWeb/assets/img/portfolio/1.jpg" alt="..." />
-                                    <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+                                    <h2 class="text-uppercase">Presencia en Línea</h2>
+                                    <p class="item-intro text-muted">Plan Básico.</p>
+                                    <img class="img-fluid d-block mx-auto" src="./assents/servicio1.png" alt="..." />
+                                    <p>*Gestión de una red social (p. ej., Facebook o Instagram).
+                                        * Publicación de contenido 3 veces por semana.
+                                        * Optimización de perfil y portada de la red social.
+                                        * Informe mensual de análisis de rendimiento.
+                                    </p>
                                     <ul class="list-inline">
                                         <li>
-                                            <strong>Client:</strong>
-                                            Threads
-                                        </li>
-                                        <li>
-                                            <strong>Category:</strong>
-                                            Illustration
+                                            <strong>Tarifa Mensual:</strong>
+                                            2,000 pesos mexicanos
                                         </li>
                                     </ul>
-                                    <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
-                                        <i class="fas fa-xmark me-1"></i>
-                                        Close Project
-                                    </button>
+                                    <a href="https://wa.me/522212145723?text=Hola engranet" target="_blank"><button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                                        <i class="fas fa-check me-1"></i>
+                                        Contactar WhatsApp
+                                    </button></a>
                                 </div>
                             </div>
                             <div class="col-lg-4">
                                 <div class="modal-body">
-                                    <h2 class="text-uppercase">Project Name</h2>
-                                    <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                                    <img class="img-fluid d-block mx-auto" src="BackWeb/assets/img/portfolio/1.jpg" alt="..." />
-                                    <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+                                    <h2 class="text-uppercase">Crecimiento Digital</h2>
+                                    <p class="item-intro text-muted">Plan Estándar.</p>
+                                    <img class="img-fluid d-block mx-auto" src="./assents/servicio2.png" alt="..." />
+                                    <p>
+                                        * Gestión de dos redes sociales (p. ej., Facebook e Instagram).
+                                        * Publicación de contenido 5 veces por semana.
+                                        * Diseño de imágenes y gráficos para redes sociales (hasta 10 por mes).
+                                        * Publicación de anuncios en Facebook Ads (presupuesto publicitario no incluido).
+                                        * Optimización de perfiles y portadas de las redes sociales.
+                                        * Informe quincenal de análisis de rendimiento.
+                                    </p>
                                     <ul class="list-inline">
                                         <li>
-                                            <strong>Client:</strong>
-                                            Threads
-                                        </li>
-                                        <li>
-                                            <strong>Category:</strong>
-                                            Illustration
+                                            <strong>Tarifa Mensual:</strong>
+                                            4,000 pesos mexicanos
                                         </li>
                                     </ul>
-                                    <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
-                                        <i class="fas fa-xmark me-1"></i>
-                                        Close Project
-                                    </button>
+                                    <a href="https://wa.me/522212145723?text=Hola engranet" target="_blank"><button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                                        <i class="fas fa-check me-1"></i>
+                                        Contactar WhatsApp
+                                    </button></a>
                                 </div>
                             </div>
                             <div class="col-lg-4">
                                 <div class="modal-body">
-                                    <h2 class="text-uppercase">Project Name</h2>
-                                    <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                                    <img class="img-fluid d-block mx-auto" src="BackWeb/assets/img/portfolio/1.jpg" alt="..." />
-                                    <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+                                    <h2 class="text-uppercase">Presencia Total en Línea</h2>
+                                    <p class="item-intro text-muted">Plan Premium.</p>
+                                    <img class="img-fluid d-block mx-auto" src="./assents/servicio3.png" alt="..." />
+                                    <p>
+                                        * Gestión de tres redes sociales (p. ej., Facebook, Instagram y Twitter).
+                                        * Publicación de contenido diario.
+                                        * Diseño de imágenes y gráficos para redes sociales (hasta 20 por mes).
+                                        * Publicación y gestión de anuncios en Facebook Ads (presupuesto publicitario incluido hasta cierto límite).
+                                        * Optimización de perfiles y portadas de las redes sociales.
+                                        * Monitoreo continuo y respuesta a comentarios y mensajes.
+                                        * Informe semanal de análisis de rendimiento.
+                                    </p>
                                     <ul class="list-inline">
                                         <li>
-                                            <strong>Client:</strong>
-                                            Threads
-                                        </li>
-                                        <li>
-                                            <strong>Category:</strong>
-                                            Illustration
+                                            <strong>Tarifa Mensual:</strong>
+                                            6,000 pesos mexicanos
                                         </li>
                                     </ul>
-                                    <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
-                                        <i class="fas fa-xmark me-1"></i>
-                                        Close Project
-                                    </button>
+                                    <a href="https://wa.me/522212145723?text=Hola engranet" target="_blank"><button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                                        <i class="fas fa-check me-1"></i>
+                                        Contactar WhatsApp
+                                    </button></a>
                                 </div>
                             </div>
 
                             <div class="col-lg-4">
                                 <div class="modal-body">
-                                    <h2 class="text-uppercase">Project Name</h2>
-                                    <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                                    <img class="img-fluid d-block mx-auto" src="BackWeb/assets/img/portfolio/1.jpg" alt="..." />
-                                    <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+                                    <h2 class="text-uppercase">Servicios Adicionales</h2>
+                                    <p class="item-intro text-muted">Costos Adicionales.</p>
+                                    <img class="img-fluid d-block mx-auto" src="./assents/servicio4.png" alt="..." />
+                                    <p>
+                                        * Creación de sitio web básico: A partir de 4,000 pesos mexicanos (dependiendo de la complejidad).
+                                        * Campañas publicitarias adicionales en Facebook Ads: Costo del presupuesto publicitario más un porcentaje (por ejemplo, el 20%) como tarifa de gestión.
+                                    </p>
                                     <ul class="list-inline">
                                         <li>
-                                            <strong>Client:</strong>
-                                            Threads
-                                        </li>
-                                        <li>
-                                            <strong>Category:</strong>
-                                            Illustration
+                                            <strong>Tarifa por proyecto:</strong>
+                                            Servicios de diseño gráfico adicionales
                                         </li>
                                     </ul>
-                                    <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
-                                        <i class="fas fa-xmark me-1"></i>
-                                        Close Project
-                                    </button>
-                                </div>
-                            </div>
-                            <div class="col-lg-4">
-                                <div class="modal-body">
-                                    <h2 class="text-uppercase">Project Name</h2>
-                                    <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                                    <img class="img-fluid d-block mx-auto" src="BackWeb/assets/img/portfolio/1.jpg" alt="..." />
-                                    <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
-                                    <ul class="list-inline">
-                                        <li>
-                                            <strong>Client:</strong>
-                                            Threads
-                                        </li>
-                                        <li>
-                                            <strong>Category:</strong>
-                                            Illustration
-                                        </li>
-                                    </ul>
-                                    <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
-                                        <i class="fas fa-xmark me-1"></i>
-                                        Close Project
-                                    </button>
-                                </div>
-                            </div>
-                            <div class="col-lg-4">
-                                <div class="modal-body">
-                                    <h2 class="text-uppercase">Project Name</h2>
-                                    <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                                    <img class="img-fluid d-block mx-auto" src="BackWeb/assets/img/portfolio/1.jpg" alt="..." />
-                                    <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
-                                    <ul class="list-inline">
-                                        <li>
-                                            <strong>Client:</strong>
-                                            Threads
-                                        </li>
-                                        <li>
-                                            <strong>Category:</strong>
-                                            Illustration
-                                        </li>
-                                    </ul>
-                                    <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
-                                        <i class="fas fa-xmark me-1"></i>
-                                        Close Project
-                                    </button>
+                                    <a href="https://wa.me/522212145723?text=Hola engranet" target="_blank"><button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                                        <i class="fas fa-check me-1"></i>
+                                        Contactar WhatsApp
+                                    </button></a>
                                 </div>
                             </div>
                         </div>
