fe0d486620db7ccb32996f0555fe997227f2a377
AustinAMTZ16 <austintv52@gmail.com>
2023-10-10 23:59:52 -0600

    Meta SEO y Meta de google analytic 4

--- index.php
+++ index.php
@@ -85,15 +85,14 @@
     '>
 
 
-
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-4R80YLVMH6"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-C7VPC57YSW"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'G-4R80YLVMH6');
+    gtag('config', 'G-C7VPC57YSW');
     </script>
 
 </head>
