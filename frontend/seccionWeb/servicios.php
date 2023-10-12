eada1629bf1ce9e63da7cfe11b6cda5f28717ce1
AustinAMTZ16 <austintv52@gmail.com>
2023-10-12 12:06:35 -0600

    Se agregan servivios nuevos

--- frontend/seccionWeb/servicios.php
+++ frontend/seccionWeb/servicios.php
@@ -13,6 +13,180 @@
                         <div class="row justify-content-center">
                             <div class="col-lg-4">
                                 <div class="modal-body">
+                                    <h2 class="text-uppercase">Paginas WEB</h2>
+                                    <p class="item-intro text-muted">Responsivas.</p>
+                                    <img class="img-fluid d-block mx-auto" src="./assents/servicio4.png" alt="..." />
+                                    <p>
+                                    <ul>
+																<li><span style="color: #333300;"><strong>1 Sitio
+																			web</strong></span></li>
+																<li><span style="color: #333300;"><strong>Dominio (.com)
+																			Gratis 1 Año</strong></span></li>
+																<li><span style="color: #333300;"><strong>Hosting 10
+																			Gigas SSD Gratis 1 Año</strong></span></li>
+																<li><span style="color: #333300;"><strong>Correos
+																			Empresariales</strong></span></li>
+																<li><span style="color: #333300;"><strong>Capacitación
+																			de Manejo</strong></span></li>
+																<li><span style="color: #333300;"><strong>Formularios de
+																			Contacto</strong></span></li>
+																<li><span style="color: #333300;"><strong>Certificado de
+																			Seguridad SSL</strong></span></li>
+																<li><span style="color: #333300;"><strong>Botón de
+																			WhatsApp</strong></span></li>
+																<li><span style="color: #333300;"><strong>100%
+																			Autoadministrable</strong></span></li>
+																<li><span style="color: #333300;"><strong>Botón de
+																			Llamada</strong></span></li>
+																<li><span style="color: #333300;"><strong>Enlaces a
+																			Redes Sociales</strong></span></li>
+																<li><span style="color: #333300;"><strong>Carga Ultra
+																			Rápida</strong></span></li>
+																<li><span style="color: #333300;"><strong>Optimización
+																			SEO</strong></span></li>
+																<li><span style="color: #333300;"><strong>Google Maps y
+																			Street View</strong></span></li>
+																<li><span style="color: #333300;"><strong>Entrega de
+																			Página Web en 5 días</strong></span></li>
+															</ul>
+                                    </p>
+                                    <!-- <ul class="list-inline">
+                                        <li>
+                                            <strong>Tarifa por proyecto:</strong>
+                                            Servicios de diseño gráfico adicionales
+                                        </li>
+                                    </ul> -->
+                                    <a href="https://wa.me/522212145723?text=Hola engranet" target="_blank"><button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button" style="background-color: #229655;">
+                                        <i class="fas fa-check me-1"></i>
+                                        Contactar WhatsApp
+                                    </button></a>
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="modal-body">
+                                    <h2 class="text-uppercase">Sitios WEB Empresas</h2>
+                                    <p class="item-intro text-muted">Responsivas.</p>
+                                    <img class="img-fluid d-block mx-auto" src="./assents/servicio4.png" alt="..." />
+                                    <p>
+                                    <ul>
+																<li><span style="color: #333300;"><strong>Hasta 10
+																			páginas dentro del Sitio web</strong></span>
+																</li>
+																<li><span style="color: #333300;"><strong>Dominio (.com)
+																			Gratis 1 Año</strong></span></li>
+																<li><span style="color: #333300;"><strong>Hosting 20
+																			Gigas SSD Gratis 1 Año</strong></span></li>
+																<li><span style="color: #333300;"><strong>Integraciones
+																			a redes sociales y pixeles</strong></span>
+																</li>
+																<li><span style="color: #333300;"><strong>Correos
+																			Empresariales</strong></span></li>
+																<li><span style="color: #333300;"><strong>Capacitación
+																			de Manejo</strong></span></li>
+																<li><span style="color: #333300;"><strong>Formularios de
+																			Contacto</strong></span></li>
+																<li><span style="color: #333300;"><strong>Certificado de
+																			Seguridad SSL</strong></span></li>
+																<li><span style="color: #333300;"><strong>Botón de
+																			WhatsApp</strong></span></li>
+																<li><span style="color: #333300;"><strong>100%
+																			Autoadministrable</strong></span></li>
+																<li><span style="color: #333300;"><strong>Botón de
+																			Llamada</strong></span></li>
+																<li><span style="color: #333300;"><strong>Enlaces a
+																			Redes Sociales</strong></span></li>
+																<li><span style="color: #333300;"><strong>Carga Ultra
+																			Rápida</strong></span></li>
+																<li><span style="color: #333300;"><strong>Optimización
+																			SEO</strong></span></li>
+																<li><span style="color: #333300;"><strong>Google Maps y
+																			Street View</strong></span></li>
+																<li><span style="color: #333300;"><strong>Entrega de
+																			Página Web en 8 días</strong></span>&nbsp;
+																</li>
+															</ul>
+                                    </p>
+                                    <!-- <ul class="list-inline">
+                                        <li>
+                                            <strong>Tarifa por proyecto:</strong>
+                                            Servicios de diseño gráfico adicionales
+                                        </li>
+                                    </ul> -->
+                                    <a href="https://wa.me/522212145723?text=Hola engranet" target="_blank"><button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button" style="background-color: #229655;">
+                                        <i class="fas fa-check me-1"></i>
+                                        Contactar WhatsApp
+                                    </button></a>
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="modal-body">
+                                    <h2 class="text-uppercase">Tiendas en linea</h2>
+                                    <p class="item-intro text-muted">ecommerce </p>
+                                    <img class="img-fluid d-block mx-auto" src="./assents/servicio4.png" alt="..." />
+                                    <p>
+                                    <ul>
+																<li><span style="color: #333300;"><strong>1 Tienda en
+																			Linea</strong></span></li>
+																<li><span style="color: #333300;"><strong>Pagos en
+																			linea: PSE, Nequi, Daviplata,PayPal,
+																			Tarjetas Débito y
+																			Crédito</strong></span><span
+																		style="color: #333300;"><strong></strong></span>
+																</li>
+																<li><span style="color: #333300;"><strong>Tienda Online
+																			Administrable</strong></span><span
+																		style="color: #333300;"><strong></strong></span>
+																</li>
+																<li><span style="color: #333300;"><strong>Carrito de
+																			compras</strong></span></li>
+																<li><span style="color: #333300;"><strong>Certificado
+																			SSL Gratuito 1 año</strong></span></li>
+																<li><span style="color: #333300;"><strong>Dominio (.com)
+																			Gratis 1 Año</strong></span></li>
+																<li><span style="color: #333300;"><strong>Hosting 10
+																			Gigas SSD Gratis 1 Año</strong></span></li>
+																<li><span style="color: #333300;"><strong>Correos
+																			Empresariales</strong></span></li>
+																<li><span style="color: #333300;"><strong>Capacitación
+																			de Manejo</strong></span></li>
+																<li><span style="color: #333300;"><strong>Formularios de
+																			Contacto</strong></span></li>
+																<li><span style="color: #333300;"><strong>Certificado de
+																			Seguridad SSL</strong></span></li>
+																<li><span style="color: #333300;"><strong>Botón de
+																			WhatsApp</strong></span></li>
+																<li><span style="color: #333300;"><strong>100%
+																			Autoadministrable</strong></span></li>
+																<li><span style="color: #333300;"><strong>Botón de
+																			Llamada</strong></span></li>
+																<li><span style="color: #333300;"><strong>Enlaces a
+																			Redes Sociales</strong></span></li>
+																<li><span style="color: #333300;"><strong>Carga Ultra
+																			Rápida</strong></span></li>
+																<li><span style="color: #333300;"><strong>Optimización
+																			SEO</strong></span></li>
+																<li><span style="color: #333300;"><strong>Google Maps y
+																			Street View</strong></span></li>
+																<li><span style="color: #333300;"><strong>Entrega de
+																			Tienda Online en 8 días</strong></span></li>
+															</ul>
+                                    </p>
+                                    <ul class="list-inline">
+                                        <li>
+                                            <strong>Tarifa por proyecto:</strong>
+                                            Servicios de diseño gráfico adicionales
+                                        </li>
+                                    </ul>
+                                    <a href="https://wa.me/522212145723?text=Hola engranet" target="_blank"><button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button" style="background-color: #229655;">
+                                        <i class="fas fa-check me-1"></i>
+                                        Contactar WhatsApp
+                                    </button></a>
+                                </div>
+                            </div>
+                            
+
+                            <div class="col-lg-4">
+                                <div class="modal-body">
                                     <h2 class="text-uppercase">Presencia en Línea</h2>
                                     <p class="item-intro text-muted">Plan Básico.</p>
                                     <img class="img-fluid d-block mx-auto" src="./assents/servicio1.png" alt="..." />
@@ -21,12 +195,12 @@
                                         * Optimización de perfil y portada de la red social.
                                         * Informe mensual de análisis de rendimiento.
                                     </p>
-                                    <ul class="list-inline">
+                                    <!-- <ul class="list-inline">
                                         <li>
                                             <strong>Tarifa Mensual:</strong>
                                             2,000 pesos mexicanos
                                         </li>
-                                    </ul>
+                                    </ul> -->
                                     <a href="https://wa.me/522212145723?text=Hola engranet" target="_blank"><button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button" style="background-color: #229655;">
                                         <i class="fas fa-check me-1"></i>
                                         Contactar WhatsApp
@@ -46,12 +220,12 @@
                                         * Optimización de perfiles y portadas de las redes sociales.
                                         * Informe quincenal de análisis de rendimiento.
                                     </p>
-                                    <ul class="list-inline">
+                                    <!-- <ul class="list-inline">
                                         <li>
                                             <strong>Tarifa Mensual:</strong>
                                             4,000 pesos mexicanos
                                         </li>
-                                    </ul>
+                                    </ul> -->
                                     <a href="https://wa.me/522212145723?text=Hola engranet" target="_blank"><button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button" style="background-color: #229655;">
                                         <i class="fas fa-check me-1"></i>
                                         Contactar WhatsApp
@@ -72,12 +246,12 @@
                                         * Monitoreo continuo y respuesta a comentarios y mensajes.
                                         * Informe semanal de análisis de rendimiento.
                                     </p>
-                                    <ul class="list-inline">
+                                    <!-- <ul class="list-inline">
                                         <li>
                                             <strong>Tarifa Mensual:</strong>
                                             6,000 pesos mexicanos
                                         </li>
-                                    </ul>
+                                    </ul> -->
                                     <a href="https://wa.me/522212145723?text=Hola engranet" target="_blank"><button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button" style="background-color: #229655;">
                                         <i class="fas fa-check me-1"></i>
                                         Contactar WhatsApp
