f407e488191141f368904f7a15c01d82f2810e34
AustinAMTZ16 <austintv52@gmail.com>
2023-10-17 00:29:26 -0600

    Update Sitio web V2

--- agendar.php
+++ agendar.php
@@ -2,7 +2,7 @@
 <html class="no-js" lang="en">
 
 <head>
-    <meta http-equiv="Content-Type; x-ua-compatible" content="text/html;charset=UTF-8; ie=edge">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Engranet</title>
     <meta name="description" content="">
--- demo.php
+++ demo.php
@@ -223,17 +223,3 @@ echo $mailSuscripcion;
 echo $mailProveedor;
 echo $mailAgendarCita;
 ?>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
